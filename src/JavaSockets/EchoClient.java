095f4156d773266288e10bc7b617b5c61d4131c1
Admin <Admin@E011-PC05.didattica.local>
2023-04-13 12:38:22 +0200

    aggiunto qualche commento

--- src/JavaSockets/EchoClient.java
+++ src/JavaSockets/EchoClient.java
@@ -9,29 +9,35 @@ import java.io.PrintWriter;
 public class EchoClient {
 	
 	public static void startClient(String ip, Integer port) throws IOException {
-		Socket socket = new Socket(ip, port);
+		
+		Socket socket = new Socket(ip, port); // inizializzazione del socket
 		System.out.println("connessione stabilita");
 		
-		Scanner socketIn = new Scanner(socket.getInputStream());
-		PrintWriter socketOut = new PrintWriter(socket.getOutputStream());
+		Scanner socketIn = new Scanner(socket.getInputStream()); // input stream del socket
+		PrintWriter socketOut = new PrintWriter(socket.getOutputStream()); // output stream del socket
 		
-		Scanner stdin = new Scanner(System.in);
+		Scanner stdin = new Scanner(System.in);	// scanner per leggere i nuovi comandi in input
 		
 		try {
+			
+			// vengono inviati i messaggi al server
 			while (true) {
-				String inputLine = stdin.nextLine();
-				socketOut.println(inputLine);
-				socketOut.flush();
+				String inputLine = stdin.nextLine(); // viene letta la stringa dal terminale
+				socketOut.println(inputLine); // viene inviato il messaggio al server tramite il socket
+				socketOut.flush(); // viene rilasciato il stream in output del socket
 				
 				String socketLine = socketIn.nextLine();
 				System.out.println(socketLine);
 			}
 			
 		} catch (NoSuchElementException e) {
+			
+			// vengono gestiti eventuali errori
 			System.out.println("connessione chiusa");
-		}
-		
-		finally {
+			
+		} finally {
+			
+			// vengono rilasciati dalla memoria tutti i processi di rete e di lettura dei dati
 			stdin.close();
 			socketIn.close();
 			socketOut.close();
@@ -42,7 +48,7 @@ public class EchoClient {
     public static void main(String[] args) throws Exception {
 
         try {
-        	startClient("localhost", 4567);
+        	startClient("localhost", 4567); // la funzione viene inserita all'interno di una closure try/catch per gestire gli eventuali errori
         } catch (IOException e) {
         	System.out.println("errore nell'avvio del client socket");
         }
--- src/JavaSockets/EchoServer.java
+++ src/JavaSockets/EchoServer.java
@@ -1,24 +1,37 @@
 package JavaSockets;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class EchoServer {
 
+	// funzione per eseguire il sever
     public static void startServer(Integer porta) throws IOException {
+    	
         // apro una porta tcp
         ServerSocket serverSocket = new ServerSocket(porta);
         System.out.println("server socket in ascolto sulla porta: " + porta);
 
         // resto in attesa di una connessione
         Socket socket = serverSocket.accept();
+        
+        BufferedReader input = new BufferedReader(new InputStreamReader(socket.getInputStream())); // salva in un buffer l'input del socket
+        
+        // esegue le operazioni finquando il socket è connesso
+        while (socket.isConnected()) {
+        	
+        	System.out.println("messaggio dal client: " + input.readLine()); // mostra i messaggi ricevuti dal client
+        }
+        
     }
 
     public static void main(String[] args) throws Exception {
 
         try {
-            startServer(4567);
+            startServer(4567); // la funzione viene inserita all'interno di una closure try/catch per gestire gli eventuali errori
         } catch (Exception e) {
             System.out.println("errore nell'avvio del server socket");
         }
